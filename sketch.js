b25f3fbb758307cd550836d9a9fb1af94968327a
anushka7849 <78260547+anushka7849@users.noreply.github.com>
2021-05-10 18:51:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,92 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var backgroundImg;
+var hour;
+var bg ;
+
+function preload() {
+    // create getBackgroundImg( ) here
+    getBackgroundImg();
+}
+
+function setup(){
+    var canvas = createCanvas(1200,700);
+    engine = Engine.create();
+    world = engine.world;
+
+}
+
+function draw(){
+
+    // add condition to check if any background image is there to add
+  if(backgroundImg) {
+background(backgroundImg);
+  }
+    Engine.update(engine);
+
+    // write code to display time in correct format here
+    textSize (20);
+if(hour>=12){
+    
+    text('Time: ' +hour%12+"pm",200,100);
+}
+else{
+    text("Time: " +hour%12+"am",200,100);
+}
+
+}
+
+async function getBackgroundImg(){
+
+    // write code to fetch time from API
+var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    //change the data in JSON format
+var responseJSON= await response.json();
+    // write code slice the datetime
+var datetime=responseJSON.datetime;
+hour=datetime.slice(11,13);
+
+    // add conditions to change the background images from sunrise to sunset
+if(hour>=04&&hour<=05){
+    bg="sunrise1.png"
+}
+else if(hour>=05&&hour<=06){
+    bg="sunrise2.png";
+}
+else if(hour>=06&&hour<=07){
+    bg="sunrise3.png";
+}
+else if(hour>=07&&hour<=08){
+    bg="sunrise4.png";
+}
+else if(hour>=08&&hour<=09){
+    bg="sunrise5.png";
+}
+else if(hour>=09&&hour<=10){
+    bg="sunrise6.png";
+}
+else if(hour>=18&&hour<=19){
+    bg="sunset7.png";
+}
+else if(hour>=19&&hour<=20){
+    bg="sunset8.png";
+}
+else if(hour>=20&&hour<=21){
+    bg="sunset9.png";
+}
+else if(hour>=21&&hour<=22){
+    bg="sunset10.png";
+}
+else if(hour>=22&&hour<=00){
+    bg="sunset11.png";
+}
+else {
+    bg="sunset12.png";
+}
+  backgroundImg=loadImage(bg) ;
+
+}
